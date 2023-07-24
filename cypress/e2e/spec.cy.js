24feed8b6c98a5e40537bc0a5c977fbc7dde49fe
ashely <thatddrgirl@gmail.com>
2023-07-24 14:55:53 -0400

    more unit tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -19,6 +19,60 @@ describe("import test: ", async () => {
   });
 });
 
+describe("import test: ", async () => {
+  it("pokemon with item only", async () => {
+    var text = `Smoochum @ Salac Berry
+    Ability: Oblivious  
+    Level: 5  
+    EVs: 236 SpA / 236 Spe  
+    Timid Nature  
+    IVs: 2 Atk / 30 SpA  
+    - Ice Beam  
+    - Psychic  
+    - Hidden Power [Grass]  
+    - Substitute`;
+
+    var pokemon = await getPokemonFromImportable(text, { speedStage: 1 });
+    expect(pokemon.name).to.equal("Smoochum");
+  });
+});
+
+describe("import test: ", async () => {
+  it("pokemon with item and gender", async () => {
+    var text = `Smoochum (F) @ Salac Berry
+    Ability: Oblivious  
+    Level: 5  
+    EVs: 236 SpA / 236 Spe  
+    Timid Nature  
+    IVs: 2 Atk / 30 SpA  
+    - Ice Beam  
+    - Psychic  
+    - Hidden Power [Grass]  
+    - Substitute`;
+
+    var pokemon = await getPokemonFromImportable(text, { speedStage: 1 });
+    expect(pokemon.name).to.equal("Smoochum");
+  });
+});
+
+describe("import test: ", async () => {
+  it("pokemon with item and nickname", async () => {
+    var text = `Abra (Smoochum) @ Salac Berry
+    Ability: Oblivious  
+    Level: 5  
+    EVs: 236 SpA / 236 Spe  
+    Timid Nature  
+    IVs: 2 Atk / 30 SpA  
+    - Ice Beam  
+    - Psychic  
+    - Hidden Power [Grass]  
+    - Substitute`;
+
+    var pokemon = await getPokemonFromImportable(text, { speedStage: 1 });
+    expect(pokemon.name).to.equal("Smoochum");
+  });
+});
+
 describe("import set: ", async () => {
   it("pokemon with no item or nickname (with gender)", async () => {
     var text = `Smoochum (F)
@@ -56,8 +110,26 @@ describe("import set: ", async () => {
   });
 });
 
+describe("import test: ", async () => {
+  it("pokemon with nickname", async () => {
+    var text = `Abra (Smoochum)
+    Ability: Oblivious  
+    Level: 5  
+    EVs: 236 SpA / 236 Spe  
+    Timid Nature  
+    IVs: 2 Atk / 30 SpA  
+    - Ice Beam  
+    - Psychic  
+    - Hidden Power [Grass]  
+    - Substitute`;
+
+    var pokemon = await getPokemonFromImportable(text, { speedStage: 1 });
+    expect(pokemon.name).to.equal("Smoochum");
+  });
+});
+
 describe("import set: ", async () => {
-  it("pokemon with item, gender, and nickname)", async () => {
+  it("pokemon with item, gender, and nickname", async () => {
     var text = `Steelix (Smoochum) (F)  
     Ability: Oblivious  
     Level: 5  
@@ -75,7 +147,7 @@ describe("import set: ", async () => {
 });
 
 describe("import set: ", async () => {
-  it("pokemon with EV (speed) and IV (not speed)", async () => {
+  it("general import test", async () => {
     var text = `Walking Wake @ Choice Specs  
     Ability: Protosynthesis  
     Tera Type: Water  
@@ -97,5 +169,95 @@ describe("import set: ", async () => {
     expect(pokemon.speedStage).to.equal(1);
     expect(pokemon.calculatedSpeed).to.equal(348);
     expect(pokemon.dexNo).to.equal(1009);
+  });
+});
+
+describe("import set: ", async () => {
+  it("set with speed EVs and speed IVs", async () => {
+    var text = `Walking Wake @ Choice Specs  
+    Ability: Protosynthesis  
+    Tera Type: Water  
+    EVs: 244 SpA / 12 SpD / 252 Spe  
+    Timid Nature  
+    IVs: 0 Spe  
+    - Hydro Steam  
+    - Flamethrower  
+    - Draco Meteor  
+    - Dragon Pulse`;
+
+    var pokemon = await getPokemonFromImportable(text, { speedStage: 1 });
+    expect(pokemon.iv).to.equal(0);
+    expect(pokemon.ev).to.equal(252);
+  });
+});
+
+describe("import set: ", async () => {
+  it("set with speed EVs", async () => {
+    var text = `Walking Wake @ Choice Specs  
+    Ability: Protosynthesis  
+    Tera Type: Water  
+    EVs: 244 SpA / 12 SpD / 252 Spe  
+    Timid Nature  
+    - Hydro Steam  
+    - Flamethrower  
+    - Draco Meteor  
+    - Dragon Pulse`;
+
+    var pokemon = await getPokemonFromImportable(text, { speedStage: 1 });
+    expect(pokemon.ev).to.equal(252);
+  });
+});
+
+describe("import set: ", async () => {
+  it("set with speed IVs", async () => {
+    var text = `Walking Wake @ Choice Specs  
+    Ability: Protosynthesis  
+    Tera Type: Water  
+    EVs: 244 SpA / 12 SpD
+    IVs: 0 Spe
+    Timid Nature  
+    - Hydro Steam  
+    - Flamethrower  
+    - Draco Meteor  
+    - Dragon Pulse`;
+
+    var pokemon = await getPokemonFromImportable(text, { speedStage: 1 });
+    expect(pokemon.iv).to.equal(0);
+  });
+});
+
+describe("import set: ", async () => {
+  it("set with no speed investment", async () => {
+    var text = `Walking Wake @ Choice Specs  
+    Ability: Protosynthesis  
+    Tera Type: Water  
+    EVs: 244 SpA / 12 SpD
+    Timid Nature  
+    - Hydro Steam  
+    - Flamethrower  
+    - Draco Meteor  
+    - Dragon Pulse`;
+
+    var pokemon = await getPokemonFromImportable(text, { speedStage: 1 });
+    expect(pokemon.ev).to.equal(0);
+    expect(pokemon.iv).to.equal(31);
+  });
+});
+
+describe("import set: ", async () => {
+  it("set with abnormal level", async () => {
+    var text = `Tandemaus @ Damp Rock  
+    Ability: Own Tempo  
+    Level: 5  
+    Tera Type: Ghost  
+    EVs: 196 Atk / 76 SpD / 236 Spe  
+    Jolly Nature  
+    - Facade  
+    - U-turn  
+    - Taunt  
+    - Rain Dance`;
+
+    var pokemon = await getPokemonFromImportable(text, { speedStage: 1 });
+    expect(pokemon.level).to.equal(5);
   });
 });
